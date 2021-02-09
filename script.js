7db7159432f231ba78eca527f062570f3dd47e70
NA-Siddiky <siddiky.academic@gmail.com>
2021-02-09 17:41:29 +0600

    adding code

--- script.js
+++ script.js
@@ -1,19 +1,29 @@
 const searchSongs = () => {
     const searchText = document.getElementById("search-item").value;
     // console.log(searchText);
-    const url = `https://api.lyrics.ovh/suggest/${searchText}`
-    // console.log(url);
-    // load data /
-    fetch(url)
-        .then(res => res.json())
-        .then(importSongs => displaySongs(importSongs.data));
-
+    const urlOfSongs = `https://api.lyrics.ovh/suggest/${searchText}`
+    // console.log(urlOfSongs);
+        fetch(urlOfSongs)
+            .then(res => res.json())
+            .then(importSongs => displaySongs(importSongs.data));
+            // .catch(error = console.log(error));
 }
+
+
+// const searchSongs = async () => {
+//     const searchText = document.getElementById("search-item").value;
+//     console.log(searchText);
+//     const urlOfSongs = `https://api.lyrics.ovh/suggest/${searchText}`
+//     const response = await fetch(urlOfSongs);
+//     const importSongs = await response.json();
+//     displaySongs(importSongs.data);
+// }
+
 
 const displaySongs = songs => {
     // console.log(songs);
     const songContainer = document.getElementById('song-container');
-    songContainer.innerHTML ="";
+    songContainer.innerHTML = "";
     songs.forEach(song => {
         // console.log(song.title)
         // console.log(song)
@@ -36,13 +46,17 @@ const displaySongs = songs => {
     })
 }
 
-const getLyric = (artist, title) => {
+const getLyric = async (artist, title) => {
     // console.log(artist, title);
     const urlOfLyric = `https://api.lyrics.ovh/v1/${artist}/${title}`;
     // console.log(urlOfLyric);
-    fetch(urlOfLyric)
-        .then(res => res.json())
-        .then(importLyric => displayLyric(importLyric.lyrics))
+    // fetch(urlOfLyric)
+    //     .then(res => res.json())
+    //     .then(importLyric => displayLyric(importLyric.lyrics))
+
+    const response = await fetch(urlOfLyric);
+    const importLyric = await response.json();
+    displayLyric(importLyric.lyrics);
 }
 
 const displayLyric = lyrics => {
